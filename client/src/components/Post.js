d9c5cd923a90dd5b05340a8c0ab394c33b0e2411
picolloo <lucaspicolloo@gmail.com>
2019-08-17 02:34:45 -0300

    Implementing Post component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Comment from "./Comment";
 
 function Post(props) {
+  const [likes, setLikes] = useState(props.likes);
+  const [comments, setComments] = useState(props.comments);
+
+  useEffect(() => {});
+
   return (
-    <div>
-      <h3>Post</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu est et
-        turpis sodales imperdiet. Sed eget consequat nisl. Sed ipsum quam,
-        ultricies eu ligula a, placerat sollicitudin sem.
-      </p>
-      <button>Like</button>
-      <button>Comment</button>
-    </div>
+    <>
+      <div>
+        <h3>{props.username}</h3>
+        <p>{props.body}</p>
+        <span>{likes}</span>
+        <button>Like</button>
+        <button>Comment</button>
+      </div>
+      <ul>
+        {comments.foreach(comment => {
+          return <Comment key={comment.id} />;
+        })}
+      </ul>
+    </>
   );
 }
 
